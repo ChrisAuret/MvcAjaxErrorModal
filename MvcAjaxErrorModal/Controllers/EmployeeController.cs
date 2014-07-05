60a1d25912f045cc5dfc001e5d7248b54111198d
chris.auret <chris.auret@yahoo.co.uk>
2014-07-05 20:42:05 +1000

    Moved project from cauret-ce to ChrisAuret

---
+++ MvcAjaxErrorModal/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxErrorModal.Controllers;
+using AutoMapper;
+using Core.Domain;
+using MvcAjaxErrorModal.ViewModels.Employee;
+using Service.Employee;
+
+namespace MvcAjaxErrorModal.Controllers
+{
+    public class EmployeeController : BaseController
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public ActionResult List()
+        {
+            var viewModel = Mapper.Map<List<EmployeeDto>, List<EmployeeViewModel>>(_employeeService.GetEmployees());
+
+            return PartialView("~/Views/Shared/Partials/Employees/_List.cshtml", viewModel);
+        }
+
+        [HttpGet]
+        public ActionResult ShowModalEmployeeList()
+        {
+           return PartialView("~/Views/Shared/Partials/Employees/_ModalShowEmployees.cshtml");
+        }
+	}
+}
---
+++ MvcAjaxErrorModal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcAjaxErrorModal.Startup))]
+namespace MvcAjaxErrorModal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
